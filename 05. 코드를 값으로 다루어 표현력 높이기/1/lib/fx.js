364db6ca0881174542ab2070148387a6cdf0ec55
Minhyuk Kim <rhrlwld@gmail.com>
2022-11-02 20:41:21 +0900

    4th

---
+++ 05. 코드를 값으로 다루어 표현력 높이기/1/lib/fx.js
@@ -0,0 +1,32 @@
+const log = console.log;
+
+// 함수를 받아서 함수를 리턴하고, 인자가 두 개 이상이면 즉시 실행, 아니면 다시 함수를 리턴하여 그 이후에 받은 인자를 받아서 다시 실행
+const curry = f =>
+  (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+const map = curry((f, iter) => {
+  let res = [];
+  for (const a of iter) {
+    res.push(f(a));
+  }
+  return res;
+});
+
+const filter = curry((f, iter) => {
+  let res = [];
+  for (const a of iter) {
+    if (f(a)) res.push(a);
+  }
+  return res;
+});
+
+const reduce = curry((f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+  return acc;
+});
